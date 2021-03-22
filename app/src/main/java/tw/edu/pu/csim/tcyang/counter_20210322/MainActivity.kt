2bf3b1fa5426e779cdf39b0a498713583cce9796
tcyang <tcyang@gm.pu.edu.tw>
2021-03-22 11:42:58 +0800

    20210322早上按鈕事件處理

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter_20210322/MainActivity.kt
@@ -0,0 +1,35 @@
+package tw.edu.pu.csim.tcyang.counter_20210322
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    var counter:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    fun Add(v:View){
+        if (v == btn1){
+            counter++
+        }
+        else if(v==btn2){
+            counter+=2
+        }
+        else{
+            counter=0
+        }
+
+        txv.text = counter.toString()
+    }
+
+
+}
